6fad3aa6b101aad78d4a5007ff314fdd66ac9003
wneddy <wnedward23@gmail.com>
2024-06-12 06:57:46 -0700

    task 0

---
+++ 0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    element = 0
+    try:
+        for count in range(x):
+            print(my_list[count], end = "")
+            element += 1
+    except IndexError:
+        pass
+    print()
+    return element
