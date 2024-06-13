0a759d4d38ee6397a07ca55708ca1a6163d503df
wneddy <wnedward23@gmail.com>
2024-06-12 23:52:59 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    first = 0
+    try:
+        for elements in range(x):
+            print("{:d}".format(my_list[elements]), end = "")
+            first =+ 1
+    except (IndexError, ValueError, TypeError):
+        pass
+    print()
+    return first
