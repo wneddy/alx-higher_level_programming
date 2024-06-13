f1e79bba29aded109e78bd49d1d78b0b037e14e1
wneddy <wnedward23@gmail.com>
2024-06-13 03:35:22 -0700

    task changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     element = 0
     try:
         for count in range(x):
-            print(my_list[count], end = "")
+            print(my_list[count], end="")
             element += 1
     except IndexError:
         pass
