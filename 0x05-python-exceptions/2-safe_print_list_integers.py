2b13ffe59baba4143febda14c449e60ce70721fe
wneddy <wnedward23@gmail.com>
2024-06-13 03:21:45 -0700

    task changes

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     first=0
     for elements in range(x):
         try:
-            print("{:d}".format(my_list[elements]), end="")
+            print("{:d}".format(my_list[elements]), end=" ")
             first += 1
         except (ValueError, TypeError):
             pass
