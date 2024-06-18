b32d8d62a3e9b1d196e68c257d9f7d5fa6cc8ebc
wneddy <wnedward23@gmail.com>
2024-06-18 00:59:35 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""File contains class Square and its set attribute"""
+"""File contains class Square and its set attribute."""
 
 
 class Square:
     """Declaration of class square"""
 
-
     def __init__(self, size):
-        """initilization of the class to set attributes
+        """Initilization of the class to set attributes.
 
         Args:
-            size (int): Square size
+            size (int): Square size.
         """
         self.__size = size
