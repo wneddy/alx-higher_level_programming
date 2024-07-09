582d7028e4cebb0ce21018c2f524c5f47ebaa5fb
wneddy <wnedward23@gmail.com>
2024-07-09 05:06:13 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,23 @@ module to add agrs passed in the command line
 and saves the list to a file
 """
 import sys
+from typing import List
+from pathlib import Path
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = \
+    __import__('6-load_from_json_file.py').load_from_json_file
 
+def main():
+    """main function"""
     try:
         args = load_from_json_file("add_item.json")
     except FileNotFoundError:
         args = []
     args.extend(sys.argv[1:])
     save_to_json_file(args, "add_item.json")
+    print(f"Added {sys.argv[1:]} to add_item.json")
+
+if __name__ == "__main__":
+    main()
