e0a93aa65b001afedf886fab268f16a561ba2787
wneddy <wnedward23@gmail.com>
2024-07-09 05:58:00 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module to create class student and define it.
+"""
+
+
+class Student():
+    """defined class"""
+    def __init__(self, first_name, last_name, age):
+        """initilizing attribute"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """method to retrieve dict"""
+        if (type(attrs) == list and
+                all(type(items)) == for item in attrs))
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
