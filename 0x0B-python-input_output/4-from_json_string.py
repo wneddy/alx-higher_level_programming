adae0c7a252211e496c161ed10cb5ff75ea1517f
wneddy <wnedward23@gmail.com>
2024-07-09 03:13:16 -0700

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module to returns an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """method to perform operation"""|
+    return json.loads(my_str)
