b248904e8c64cab8d3c87e75ef5261ce23031aa9
wneddy <wnedward23@gmail.com>
2024-07-09 02:48:47 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module to that writes a string to a
+text file (UTF8) and returns the number
+of characters written"""
+
+
+def write_file(filename="", text=""):
+    """method to perfome the operation"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        output = f.write(text)
+        print(output)
+        
