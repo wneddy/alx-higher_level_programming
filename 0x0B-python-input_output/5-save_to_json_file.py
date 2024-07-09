2ee2b1b7892dc59e1d2e1bbcd9cef6a8d4145bc8
wneddy <wnedward23@gmail.com>
2024-07-09 03:32:41 -0700

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to writes an Object to a text
+file, using a JSON representation
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """"method to perform the operation"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return json.dumps(my_obj, f)
