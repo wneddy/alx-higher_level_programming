92b8af6672d10c9fc4fda741f332f83382f8f79a
wneddy <wnedward23@gmail.com>
2024-07-09 05:19:04 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,12 @@ module to add agrs passed in the command line
 and saves the list to a file
 """
 import sys
-from typing import List
-from pathlib import Path
 
 
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = \
     __import__('6-load_from_json_file.py').load_from_json_file
+
 
 def main():
     """main function"""
@@ -21,6 +20,7 @@ def main():
     args.extend(sys.argv[1:])
     save_to_json_file(args, "add_item.json")
     print(f"Added {sys.argv[1:]} to add_item.json")
+
 
 if __name__ == "__main__":
     main()
