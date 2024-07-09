fbb38707d85939b23c4e8e604798946dc6ee1509
wneddy <wnedward23@gmail.com>
2024-07-09 07:13:35 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+module to create class student and define it.
+"""
+
+
+class Student():
+    """defined class"""
+    def __init__(self, first_name, last_name, age):
+        """initilizing attribute"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method to retrieve dict"""
+        if (isinstance(attrs, list) and
+                all(isinstance(item, str)) for item in attrs):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace attribute for students"""
+        for k, v in json.items():
+            setattr(self, k, v)
