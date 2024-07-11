b180e2a1480806c62f4e113880ed372ff23f8086
wneddy <wnedward23@gmail.com>
2024-07-11 02:55:15 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module to create a pascal triangle in form a list
+"""
+
+
+def pascal_triangle(n):
+    """the method to conduct the operation"""
+    if n <= 0:
+        return []
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
