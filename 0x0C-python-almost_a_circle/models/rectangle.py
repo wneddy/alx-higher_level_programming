6c3b51ae4499d34c2850b5cbe6ea5a39a272dae6
wneddy <wnedward23@gmail.com>
2024-07-15 07:10:47 -0700

    rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+module to create class Rectangle inheriting from base class
+"""
+
+
+class Rectangle(Base):
+    """Rectangle class created"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialization method for all the attributes"""
+        self.__width = width
+        self.__height = height
+        self. __x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """get width"""
+        return self.width
+
+    @property
+    def height(self):
+        """get height"""
+        return self.height
+
+    @property
+    def x(self):
+        """get x"""
+        return self.x
+
+    @property
+    def y(self):
+        """get y"""
+        return self.y 
