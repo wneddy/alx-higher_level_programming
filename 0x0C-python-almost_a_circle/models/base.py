911eccf8f584268896e0b9951d3ab8785c1a2de6
wneddy <wnedward23@gmail.com>
2024-07-18 01:09:14 -0700

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """converts dict to json(str) format"""
